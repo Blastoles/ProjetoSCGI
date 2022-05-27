5f90398ac7bd3ce3484857fd6e00ca890e85ad72
Igor C <igornovo5995@gmail.com>
2022-05-26 21:29:31 -0300

    atualizando

--- View/ViewRelatorio.py
+++ View/ViewRelatorio.py
@@ -1,6 +1,12 @@
 from os import getcwd
 from collections import OrderedDict
 from PyQt5 import uic, QtWidgets
+import numpy as np
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import *
+import sys
+import pyqtgraph as pg
+
 
 class viewRelatorio():
     def Show(self):
@@ -54,8 +60,27 @@ class viewRelatorio():
             return (list(OrderedDict.fromkeys(lst)))
 
 
+    def GraficoRelatorioBar(self):
+        widget = QWidget()
+        btn = QPushButton('Push Button')
+        text = QLineEdit("Line Edit")
+        check = QCheckBox("Check Box")
+        plot = pg.plot()
+        y1 = [5, 5, 7, 10, 3, 8, 9, 1, 6, 2]
+        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        bargraph = pg.BarGraphItem(x=x, height=y1, width=0.6, brush='g')
+        plot.addItem(bargraph)
+        layout = QGridLayout()
+        widget.setLayout(layout)
+        layout.addWidget(btn, 0, 0)
+        layout.addWidget(text, 1, 0)
+        layout.addWidget(check, 3, 0)
+        layout.addWidget(plot, 0, 1, 3, 1)
+        self.tela.setCentralWidget(widget)
+
     def __init__(self):
-        Local = getcwd()
-        Local = Local.split('Controller')
-        Local = Local[0].replace('C:','C:\\')
-        self.tela = uic.loadUi("{}View\Telas\CD_Relatorio.ui".format(Local))
+            Local = getcwd()
+            Local = Local.split('Controller')
+            Local = Local[0].replace('C:', 'C:\\')
+            self.tela = uic.loadUi("{}View\Telas\CD_Relatorio.ui".format(Local))
+
