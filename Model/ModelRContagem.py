4f09952e3402658bfd66b962a531d629b57d898c
Igor C <igornovo5995@gmail.com>
2022-07-07 15:45:09 -0300

    atualizando

--- Model/ModelRContagem.py
+++ Model/ModelRContagem.py
@@ -6,17 +6,14 @@ class ModelRContagem():
         self = GRela
         self.tela.TB_Contagem.clearContents()
         n = 0
-        ContImpre = 0
         nLista = len(lista)
         self.tela.TB_Contagem.setRowCount(nLista)
         for i in range(0, nLista):
             for j in range(0, 3):
-                if j == 1:
+                if j != 2:
                     self.Lista(i, j, str(lista[i][j]))
-                    if i == nLista-1:
-                        self.Lista(i, j+2, str("Dado de Comparação"))
-                    else:
-                        self.Lista(i, j+2, str(lista[i][j] - lista[i + 1][j]))
                 else:
-                    self.Lista(i, j, str(lista[i][j]))
+                    self.Lista(i, j + 1, str(lista[i][j]))
+                    if i != nLista-1:
+                        self.Lista(i, j, str(lista[i][j - 1] - lista[i + 1][j - 1]))
                 n += 1
